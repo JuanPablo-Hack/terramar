798ccb58c31a2685732593138c0d8136f7b98583
Juan Pablo Hack <juanpablodejesus_figueroa@ucol.mx>
2021-05-10 09:48:10 -0500

    Subida de pagina al dominio

---
+++ carga.js
@@ -0,0 +1,8 @@
+window.addEventListener('load',()=>{
+    setTimeout(cargar,4000);
+    function cargar(){
+    document.getElementById('contenido').style.display = "inherit";
+    document.getElementById('carga').style.display = "none"; 
+    }
+    
+})
